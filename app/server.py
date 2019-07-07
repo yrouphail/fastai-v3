7e893cc92d79c54af24e91ca1f14ebd13a9f6f51
yrouphail <52511399+yrouphail@users.noreply.github.com>
2019-07-07 18:00:00 -0400

    Update server.py

--- app/server.py
+++ app/server.py
@@ -60,8 +60,8 @@ async def analyze(request):
     img_data = await request.form()
     img_bytes = await (img_data['file'].read())
     img = open_image(BytesIO(img_bytes))
-    prediction = learn.predict(img)[0]
-    return JSONResponse({'result': str(prediction)})
+    pred_class,pred_idx,outputs = learn.predict(img)
+    return JSONResponse('Atelectasis{'result': str(outputs)})
 
 
 if __name__ == '__main__':
